de6d2a13d274fddfce0905594f811b467a281339
Keegan Farley <keegansfarley@gmail.com>
2018-08-23 18:46:48 -0700

    Adding an ES6 only branch (#229)
    * Fix links in example and readme (#206)
    
    * Report error from ColladaLoader in MeshResource (#210)
    
    this will hopefully make situations like #209 easier to debug
    
    * Added Rollup
    
    * Added yarn lockfile
    
    * Removed references to COLLADA_LOADER_2
    
    * Added es6 transpiler and rollup config, working on transpiling
    
    * Touched up a couple potential mistypes prevent transpiler from working. 1 - removed explicit super.super() call from InteractiveMarker. 2 - Made Particles explicitly derive from THREE.Object3D.
    
    * Rewrote Particles' method signatures in format consistent with rest of codebase
    
    * Refactored updateMatrixWorld to be more statically analyzable
    
    * ES6 modules properly compiling, Working on runtime errors
    
    * Moved 'that' assignments happen *after* calls super constructors in derived classes
    
    * Added missing super constructor call
    
    * Removed assignment to read-only property causing a runtime error, added a relevant comment
    
    * Moved all super constructor calls to preceed any use of 'this'
    
    * Added shims for THREE and THREE extensions to support es6 compatible module extensions
    
    * Cleanup and added examples for single page applications and using html imports in browsers
    
    * Moved es6 support files to es6-support folder and renamed destination for es6 transpiled output
    
    * Moved shims to top level
    
    * Removed pre-es6 source code and es6 transpiler
    
    * DepthCloud: make depth range adaptable + fix depth point position decoding (#207)
    
    * PointCloud2 and LaserScan (#218)
    
    * buffergeometry in PointCloud2
    * pointcloud2: buffergeometry, subsampling
    * renamed Particles.js to Points.js, deprecation warning in Points.js
    * Update kitti.html
    * Removing redundant roslaunch calls in the help.
    
    * NavSatFix support (#221)
    
    * fix: pointRatio option in PointCloud2 and LaserScan (#223)
    
    * Added build products
    
    * Removed extra log
    
    * Added angular app example
    
    * Added react app example and touched up other SPA examples
    
    * Added build output
    
    * Removed extra log
    
    * Added angular app example
    
    * Added react app example and touched up other SPA examples
    
    * Updated ROSLIB import semantics
    
    * Updated ROSLIB import semantics
    
    * Switched ColladaLoader shim from official ColladaLoader to the ros3djs fork
    
    * Switched ColladaLoader shim from official ColladaLoader to the ros3djs fork
    
    * Updated html-import example to use yarn like the other examples
    
    * Updated html-import example to use yarn like the other examples
    
    * Updated outdated grunt plugins
    
    * Fixed linter errors
    
    * Updated grunt build process to build es6 output
    
    * Switched from jshint to eslint, migrating rules and fixing lint issues
    
    * Updated commonjs target to be es5
    
    * Fixed pkg.module to use es6 module syntax, but es5 language features
    
    * Fixed bug in Points class
    
    * Switched from const to var for es5 compatibility
    
    * Added PointCloud2 example for angular app
    
    * Updated node version use by CI server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,39 @@
+
+// Export Grunt config
 module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      build: {
-        src  : ['./src/*.js', './src/**/*.js'],
-        dest : './build/ros3d.js'
+    eslint: {
+      lint: {
+        options: {
+          configFile: '.eslintrc',
+        },
+        src: [
+          'Gruntfile.js',
+          './src/*.js',
+          './src/**/*.js',
+          './tests/*.js'
+        ],
+      },
+      fix: {
+        options: {
+          configFile: '<%= eslint.lint.options.configFile  %>',
+          fix: true
+        },
+        src: '<%= eslint.lint.src  %>',
       }
     },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      files: [
-        'Gruntfile.js',
-        './build/ros3d.js',
-        './tests/*.js'
-      ]
+    shell: {
+      build: {
+        command: 'rollup -c'
+      }
     },
     karma: {
       build: {
         configFile: './test/karma.conf.js',
         singleRun: true,
         browsers: ['PhantomJS']
-      }
-    },
-    uglify: {
-      options: {
-        report: 'min'
-      },
-      build: {
-        src: './build/ros3d.js',
-        dest: './build/ros3d.min.js'
       }
     },
     watch: {
@@ -51,7 +53,7 @@ module.exports = function(grunt) {
         },
         files: [
           'Gruntfile.js',
-          '.jshintrc',
+          '.eslintrc',
           './src/*.js',
           './src/**/*.js'
         ],
@@ -79,15 +81,19 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-pipe');
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('gruntify-eslint');
 
   grunt.registerTask('dev', ['concat', 'watch']);
-  grunt.registerTask('build', ['concat', 'jshint', 'uglify']);
+  grunt.registerTask('build', ['eslint:lint', 'shell']);
   grunt.registerTask('build_and_watch', ['watch']);
   grunt.registerTask('doc', ['clean', 'jsdoc']);
+  grunt.registerTask('lint', ['eslint:lint',]);
+  grunt.registerTask('lint-fix', ['eslint:fix',]);
 };
